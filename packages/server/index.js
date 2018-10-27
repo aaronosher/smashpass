534d2500776935ce29d1f4fe7470523941c59c8f
pancrates <pancrates@protonmail.com>
2018-10-28 00:40:57 +0100

    new stuf

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,7 +4,7 @@ var express = require("express"),
     fs = require('fs'),
     util = require('./util/util'),
     pth = require('path');
-
+const { exec } = require('child_process');
 
 var app = express();
 
@@ -16,15 +16,93 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log(__dirname);
 
+////////////////////////////////////////
+
+function runNN(comand,wPath,inPath,alpha,count,layers,callback){
+  exec(`${comand} ${wPath} ${inPath} ${alpha} ${count} ${layers}`, (err, stdout, stderr) => callback(err,stdout,stderr));
+}
+
+function getFileData(path){
+  var data = fs.readFileSync(path);
+}
+
+function mixMeSomeTrainingData(uFile, smashLibPath){
+  var uFileData =  fs.readFileSync(smashLibPath+"/"+uFile, 'utf8');
+  var libFiles = util.getAllFilesSync("userData/smashLib");
+  var trainingData=[];
+  uFileData = uFileData.split("\n");
+  var linesToTake = uFileData.length/libFiles.length;
+  uFileData=  uFileData.map(line => {
+    if(line.length<5) return;
+    else return line.concat(",y")
+  });
+
+  trainingData+=uFileData.join("\n");
+  //console.log(trainingData);
+    for(var i=0;i<libFiles.length;i++){
+      var fName = libFiles[i]
+      if(uFile==fName) {
+        console.log("sucessful skip")
+        continue;
+      }
+      var contents = fs.readFileSync(smashLibPath+"/"+fName, 'utf8');
+      //console.log(contents);
+      var contArray = contents.split("\n");
+      //console.log(contArray);
+      contArray=  contArray.map(line => {
+        if(line.length<5) return;
+        else return line.concat(",n")
+      });
+      //console.log(contArray);
+      shuffleUsingSortFunc(contArray);
+      //console.log(contArray.slice(0,linesToTake));
+      contArray = contArray.slice(0,linesToTake)
+      contArray[contArray.length-1]+="\n";
+      trainingData+=contArray.join("\n");
+    }
+  return trainingData
+}
+
+function shuffleUsingSortFunc(array){
+ array.sort(function(){
+   return 0.5-Math.random();
+ });
+}
+function storeFile(uid,path,data){
+  fs.writeFileSync(`${path}/${uid}.txt`, trainingFile);
+}
+
+
+function trainCallback(err,stdout,stderr){
+  if(err){
+    console.log("trainCallback: Error "+ err);
+    return {err:true,errMsg:err};
+  }
+  console.log(stdout)
+  return {stdout}
+}
+///////////////////////////////////////
+
+runNN("python3 packages/ml/classifier.smash.py train-fresh","userData/testW.json","userData/smashSample.txt","0.3","1000"," 10",trainCallback);
+
+
+
+
+
 app.get("/", function (req, res) {
   var body = req.body;
   res.send("hello");
 });
+app.post("/register", function (req, res) {
+  var body = req.body;
+  res.send("hello "+ body);
+});
 
+/*
 app.post("/login", function (req, res) {
   var body = req.body; //uid+smash
   ///
-  data={isApproved:false,conficence:/*(sonefloat)*/}
+  data={isApproved:false,conficence:(sonefloat)}
   res.send(body);
 });
 app.post("/register", function (req, res) {
@@ -34,9 +112,9 @@ app.post("/register", function (req, res) {
   res.send(body);
 });
 
+*/
 
 
+//app.listen(80).on('error', function(err) { console.log("err")});
 
-app.listen(80).on('error', function(err) { console.log("err")});
-
-console.error("--------", new Date(), "--------");
+//console.error("--------", new Date(), "--------");
--- packages/server/test.js
+++ packages/server/test.js
@@ -20,18 +20,59 @@ function runNN(comand,wPath,inPath,alpha,count,layers){
 });
 }
 
+function getFileData(path){
+  var data = fs.readFileSync(path);
+}
+
 function mixMeSomeTrainingData(uFile, smashLibPath){
-  var uFileData;
-  fs.readFile(uFile, function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    uFileData = data;
+  var uFileData =  fs.readFileSync(smashLibPath+"/"+uFile, 'utf8');
+  var libFiles = util.getAllFilesSync("userData/smashLib");
+  var trainingData=[];
+  uFileData = uFileData.split("\n");
+  var linesToTake = uFileData.length/libFiles.length;
+  uFileData=  uFileData.map(line => {
+    if(line.length<5) return;
+    else return line.concat(",y")
   });
 
-
+  trainingData+=uFileData.join("\n");
+  //console.log(trainingData);
+    for(var i=0;i<libFiles.length;i++){
+      var fName = libFiles[i]
+      if(uFile==fName) {
+        console.log("sucessful skip")
+        continue;
+      }
+      var contents = fs.readFileSync(smashLibPath+"/"+fName, 'utf8');
+      //console.log(contents);
+      var contArray = contents.split("\n");
+      //console.log(contArray);
+      contArray=  contArray.map(line => {
+        if(line.length<5) return;
+        else return line.concat(",n")
+      });
+      //console.log(contArray);
+      shuffleUsingSortFunc(contArray);
+      //console.log(contArray.slice(0,linesToTake));
+      contArray = contArray.slice(0,linesToTake)
+      contArray[contArray.length-1]+="\n";
+      trainingData+=contArray.join("\n");
+    }
+  return trainingData
 }
-  console.log(util.getAllFilesSync("userData/smashLib"));
+
+function shuffleUsingSortFunc(array){
+ array.sort(function(){
+   return 0.5-Math.random();
+ });
+}
+function storeFile(uid,path,data){
+  fs.writeFileSync(`${path}/${uid}.txt`, trainingFile);
+}
+//console.log(getFileData("userData/smashSampleTest.txt"));
+//  console.log(util.getAllFilesSync("userData/smashLib"));
+var trainingFile = mixMeSomeTrainingData("aaronSmash.txt","userData/smashLib")
+storeFile("aaron","userData/trainData",trainingFile);
 
 //runNN("python3 packages/ml/classifier.smash.py train-fresh","userData/testW.json","userData/smashSample.txt","0.3","1000"," 10");
 //runNN("python3 packages/ml/classifier.smash.py validate","userData/testW.json","userData/smashSampleTest.txt"," "," "," " );
