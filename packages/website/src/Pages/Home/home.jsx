91b8d3d0f629722a5e1cf65837031b24d5dbd550
Aaron Osher <aaron@oshers.com>
2018-10-28 04:38:28 +0000

    Add Nav Links to Home

--- packages/website/src/Pages/Home/home.jsx
+++ packages/website/src/Pages/Home/home.jsx
@@ -1,13 +1,49 @@
-import React, { Component } from 'react'
 
-class home extends Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import styles from './styles';
+
+class Home extends React.Component {
+
   render() {
+    const { classes, navigate } = this.props;
+
     return (
-      <div>
-        Hello World
-      </div>
-    )
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar position="absolute" color="default" className={classes.root}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap className={classes.grow}>
+              SmashPass IDP
+            </Typography>
+            <Button color="inherit" onClick={navigate('/login')}>Login</Button>
+            <Button color="inherit" onClick={navigate('/signUp')}>Sign Up</Button>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
+    );
   }
 }
 
-export default home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({
+  navigate: (path) => () =>  dispatch(push(path)),
+});
+
+export default compose(
+  withStyles(styles, { withTheme: true }),
+  connect(mapStateToProps, mapDispatchToProps),
+)(Home);
