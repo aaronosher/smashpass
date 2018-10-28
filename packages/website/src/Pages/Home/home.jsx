4ac733590daac85f9def84cb1703a7bf085ae4de
Aaron Osher <aaron@oshers.com>
2018-10-28 11:58:13 +0000

    Add Home Page Message

--- packages/website/src/Pages/Home/home.jsx
+++ packages/website/src/Pages/Home/home.jsx
@@ -10,7 +10,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import styles from './styles';
+import banner from '../../Images/banner.png';
 
 class Home extends React.Component {
 
@@ -29,6 +31,26 @@ class Home extends React.Component {
             <Button color="inherit" onClick={navigate('/signUp')}>Sign Up</Button>
           </Toolbar>
         </AppBar>
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <img src={banner} alt="SmashPass Banner" width={200} className={classes.banner} />
+            <Typography variant="h6" component="h6" gutterBottom>
+              Welcome to SmashPass
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              This is SmashPass, a novelty way for password-less authentication.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              <strong>What it does?</strong><br />
+              SmashPass is a password-less Identity Provider that uses machine learning to create a model of your characteristic smashing patterns. We use the model to authenticate you, and only you, every single time.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              <strong>How it works?</strong><br />
+              Using a small neural network, SmashPass identifies patterns in your keyboard *smashing* which are then used to build a model allowing you to authenticate by caressing the keyboard in your own unique way.
+            </Typography>
+            <Button color="primary" onClick={navigate('/signUp')}>Sign Up</Button>
+          </Paper>
+        </main>
       </React.Fragment>
     );
   }
--- packages/website/src/Pages/Home/styles.js
+++ packages/website/src/Pages/Home/styles.js
@@ -1,4 +1,33 @@
 const styles = theme => ({
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    marginTop: 100,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
@@ -9,6 +38,13 @@ const styles = theme => ({
   grow: {
     flexGrow: 1,
   },
+  banner: {
+    left: '-24px',
+    position: 'relative',
+    top: '-16px',
+    width: '400px',
+  }
 });
+
 
 export default styles;
