25da77d59adb49d0ad4f8864bd0ed95a44954290
pancrates <pancrates@protonmail.com>
2018-10-28 01:15:43 +0100

    new stuf

--- packages/server/index.js
+++ packages/server/index.js
@@ -99,9 +99,11 @@ app.post("/register", function (req, res) {
   var smashLog = body.smashes.join("\n");
   var uid = body.uid;
 
-  console.log(uid+ " smashes\n "+smashLog)
+  //console.log(uid+ " smashes\n "+smashLog)
   //smashLog = smashLog.split("\n");
   storeFile(uid,"userData/smashLib",smashLog);
+  var trainingFile = mixMeSomeTrainingData(`${uid}.txt`,"userData/smashLib")
+  storeFile(`${uid}`,"userData/trainData",trainingFile);
   //runNN(`python3 packages/ml/classifier.smash.py train-fresh`,`userData/${uid}W.json`,`userData/trainData/${uid}.txt`,"0.3","1000"," 10",trainCallback);
 
   res.send("hello "+ body);
